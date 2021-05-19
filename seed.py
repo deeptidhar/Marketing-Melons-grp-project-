67b0e22216e5566e8755efe4bcda495ec086e5a2
vagrant <vagrant@vagrant.vm>
2021-05-19 15:58:03 +0000

    seed.py update to use csv for melon categories

--- seed.py
+++ seed.py
@@ -6,6 +6,8 @@ import crud
 import model 
 import os
 
+import csv
+
 # import server
 from faker import Faker
 
@@ -22,3 +24,14 @@ for _ in range(20): #can you do `for i in range(20)`?
     name = faker.name()
     print(email, password, name)
     crud.create_user(email, password, name)
+ 
+with open('melon_categories.csv', newline='') as categories_csv:
+    categories_reader = csv.DictReader(categories_csv)
+    for row in categories_reader:
+        name = row['name']
+        color = row['color']
+        is_seedless = row['is_seedless']
+        melon_img_url = row['melon_img_url']
+
+        crud.create_melon_category(name, color, is_seedless, melon_img_url)
+        crud.create_melon_category(row['name'], row['color'], row['is_seedless'], row[melon_img_url])
