79e580853f72269f6f88b495b755e9c726fe5899
Sarah <sarahhtbarton@gmail.com>
2021-05-20 14:36:36 +0000

    Successfully seeds user and category tables

--- seed.py
+++ seed.py
@@ -11,12 +11,15 @@ import csv
 import server
 from faker import Faker
 
+import ast #to convert "True" and "False" strings from the csv to boolean values
+
 os.system('dropdb marketplace')
 os.system('createdb marketplace')
 
 model.connect_to_db(server.app)
 model.db.create_all()
 
+#seeds users table
 faker = Faker()
 for _ in range(20):
     email = faker.email()
@@ -24,7 +27,9 @@ for _ in range(20):
     name = faker.name()
     crud.create_user(email, password, name)
  
-with open('melon_categories.csv', newline='') as categories_csv:
+#seeds category table
+with open('data/melon_categories.csv', newline='') as categories_csv:
     categories_reader = csv.DictReader(categories_csv)
     for row in categories_reader:
-        crud.create_melon_category(row['is_seedless'], row['name'], row['color'], row['melon_img_url'])
+        crud.create_melon_category(ast.literal_eval(row['is_seedless']), row['name'], row['color'], row['melon_img_url'])
+
