5f6a095ee3c1a170683b9657551a6443c62f74fb
Sarah <sarahhtbarton@gmail.com>
2021-05-20 15:45:24 +0000

    Starts seeding melon_listing table with test data

--- seed.py
+++ seed.py
@@ -33,3 +33,16 @@ with open('data/melon_categories.csv', newline='') as categories_csv:
     for row in categories_reader:
         crud.create_melon_category(ast.literal_eval(row['is_seedless']), row['name'], row['color'], row['melon_img_url'])
 
+#seeds melon_listings table with test data
+with open('data/test_data_melon_listings.csv', newline='') as listings_csv:
+    listings_reader = csv.DictReader(listings_csv)
+    for row in listings_reader:
+        print(row['name'])
+        print(row['seller_id'])
+        print(row['winner_id'])
+        end_date_test = row['end_date']
+        print(row['end_date'])
+        print(row['description'])
+        print(row['melon_category'])
+        print(ast.literal_eval(row['is_sold']))
+        #crud.create_melon_listing(row['name'], row['seller_id'], row['winner_id'], row['end_date'], row['description'], row['melon_category'], ast.literal_eval(row['is_sold']))
