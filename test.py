089ec4bf02d88992c94b44adbae7fdb02c88e889
Quanisha Williams <ms_q_williams@gmail.com>
2021-05-21 15:39:32 +0000

    finished testing all table in the db

--- test.py
+++ test.py
@@ -26,10 +26,10 @@ class FlaskTestsDatabase(TestCase):
         # Create tables and add sample data
         db.create_all()
 
-       # self.user = crud.create_user(email='LarryJackson278@yahoo.com', password ='SgK167xD', name ='Larry Jackson')
-        #self.melon_category = crud.create_melon_category(is_seedless = False, name='Royal Golden Watermelon' , color='gold', melon_img_url='https://www.rareseeds.com/store/vegetables/watermelon/royal-golden-watermelon')
-        self.melon_listing = crud.(name='', seller_id='', winner_id= '', end_date= '', description = 'One of the best', melon_category='', is_sold = False)
-        #self.bid = crud.create_bid(user_id = 'users.user_id' , melon_id = '', bid_amount='9.50', timestamp='05/30/2021')
+        self.user = crud.create_user(email='LarryJackson278@yahoo.com', password ='SgK167xD', name ='Larry Jackson')
+        self.melon_category = crud.create_melon_category(is_seedless = False, name='Royal Golden Watermelon' , color='gold', melon_img_url='https://www.rareseeds.com/store/vegetables/watermelon/royal-golden-watermelon')
+        self.melon_listing = crud.create_melon_listing(name='Delicious Green Melon', seller_id=None, winner_id= None, end_date= '05/22/2021 01:30:15', description = 'One of the best', melon_category=None, is_sold = False)
+        self.bid = crud.create_bid(user_id = None , melon_id = None, bid_amount=9.50, timestamp='05/30/2021')
 
         
     def tearDown(self):
@@ -40,48 +40,48 @@ class FlaskTestsDatabase(TestCase):
         db.engine.dispose()
         
     ######################################################TESTING USER TABLE####################################################################    
-    # def test_user_creation(self):
-    #     """Test user profile page and to make sure user object 
-    #     has been created successfully"""
+    def test_user_creation(self):
+        """Test user profile page and to make sure user object 
+        has been created successfully"""
         
-    #     self.assertIsInstance(self.user, User)
+        self.assertIsInstance(self.user, User)
         
-    # def test_email(self):
-    #     """Testing that the user object 'email' has been constructed properly"""
+    def test_email(self):
+        """Testing that the user object 'email' has been constructed properly"""
         
-    #     self.assertEqual('LarryJackson278@yahoo.com', self.user.emaiL)
+        self.assertEqual('LarryJackson278@yahoo.com', self.user.email)
     
     ######################################################TESTING MELON CATEGORY TABLE####################################################################
-    # def test_melon_category_creation(self):
-    #     """Test melon category table and to make sure melon object has been created successfully"""
+    def test_melon_category_creation(self):
+        """Test melon category table and to make sure melon object has been created successfully"""
         
-    #     self.assertIsInstance(self.melon_category, MelonCategory)
+        self.assertIsInstance(self.melon_category, MelonCategory)
         
-    # def test_melon_name(self):
-    #     """Testing that the melon category object 'name' has been constructed properly"""
+    def test_melon_name(self):
+        """Testing that the melon category object 'name' has been constructed properly"""
         
-    #     self.assertEqual('Royal Golden Watermelon', self.melon_category.name)
+        self.assertEqual('Royal Golden Watermelon', self.melon_category.name)
     #########################################################TESTING MELON LISTING TABLE####################################################################
-     def test_melon_listing_creation(self):
-            """Test melon listing table and to make sure melon listing object has been created successfully"""
+    def test_melon_listing_creation(self):
+        """Test melon listing table and to make sure melon listing object has been created successfully"""
         
         self.assertIsInstance(self.melon_listing, MelonListing)
         
     def test_melon_description(self):
         """Testing that the melon listing object 'description' has been constructed properly"""
         
-        self.assertEqual('', self.melon_listing.description)
+        self.assertEqual('One of the best', self.melon_listing.description)
 
     ################################################################TESTING BID TABLE####################################################################
-    # def test_bid_creation(self):
-    #     """Test bid table and to make sure bid object has been created successfully"""
+    def test_bid_creation(self):
+        """Test bid table and to make sure bid object has been created successfully"""
         
-    #     self.assertIsInstance(self.bid, Bid)
+        self.assertIsInstance(self.bid, Bid)
         
-    # def test_bid_amount(self):
-    #     """Testing that the bid object 'bid amount' has been constructed properly"""
+    def test_bid_amount(self):
+        """Testing that the bid object 'bid amount' has been constructed properly"""
         
-    #     self.assertEqual('9.50', self.bid.bid_amount)
+        self.assertEqual(9.50, self.bid.bid_amount)
 
 
 
