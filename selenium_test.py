10b787871961d1cb7f5c6cadf2d659fead8c66a6
Quanisha Williams <ms_q_williams@gmail.com>
2021-05-26 21:25:36 +0000

    added more test to selenium

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,11 @@
 import os
 import unittest  
 import time 
+import crud
 import json
+from seed import create_example_data
+from server import app
+from model import db, connect_to_db
 from selenium import webdriver  
 from selenium.webdriver.chrome.options import Options  
 
@@ -14,15 +18,17 @@ class TestBidMelon(unittest.TestCase):
     def setUp(self):
         """Stuff to do before every test."""
 
-         # Get the Flask test client
-        self.client = app.test_client()
+         
         app.config['TESTING'] = True
 
         # Connect to test database
         connect_to_db(app, "postgresql:///test_db")
 
         # Create tables and add sample data
+        db.drop_all()
         db.create_all()
+        create_example_data()
+        crud.create_user('qwilliams@yahoo.com', '12345678', 'Quanisha Williams')
 
         self.browser = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), 
                                         options=chrome_options)
@@ -36,31 +42,64 @@ class TestBidMelon(unittest.TestCase):
         db.engine.dispose()
         
 
+    #This test works
+    # def test_title(self):
+    #     """Testing title of browser"""
 
-    def test_title(self):
-        self.browser.get('http://localhost:5000/')
-        self.assertEqual(self.browser.title, 'BitMelon - Bid on a Melon')
+    #     self.browser.get('http://localhost:5000/')
+    #     self.assertEqual(self.browser.title, 'BitMelon - Bid on a Melon')
 
-    def test_login(self):
-        self.browser.get('http://localhost:5000/login')
+    #This test works git 
+    # def test_login_pass(self):
+    #     """Testing user login is successful"""
 
-        email = browser.find_element_by_id('email')
-        email.send_keys("test123@test.com")
-        password = browser.find_element_by_id('password')
-        password.send_keys("Abc1234")
+    #     self.browser.get('http://localhost:5000/login')
+    #     time.sleep(10)
 
-        btn = self.browser.find_element_by_xpath("//input[@type='submit']")
+    #     email = self.browser.find_element_by_id('email')
+    #     email.send_keys("qwilliams@yahoo.com")
+    #     password = self.browser.find_element_by_id('password')
+    #     password.send_keys("12345678")
 
-        btn.click()
+    #     btn = self.browser.find_element_by_xpath("//input[@type='submit']")
 
-        time.sleep(2)
+    #     btn.click()
 
-        alert = self.browser.switch_to.alert
-        self.assertEqual(alert.text, "Login successful")
-        alert.accept()    
-        print(" Clicked on the OK Button in the Alert Window")
-        alert.close
+    #     time.sleep(2)
+
+    #     alert = self.browser.switch_to.alert
+    #     self.assertEqual(alert.text, "Login successful")
+    #     alert.accept()    
+    #     print(" Clicked on the OK Button in the Alert Window")
+        
+    
+    #This test works 
+    # def test_login_failed(self):
+    #     """Testing user login failed"""
+
+    #     self.browser.get('http://localhost:5000/login')
+    #     time.sleep(10)
+
+    #     email = self.browser.find_element_by_id('email')
+    #     email.send_keys("mtate@hotmail.com")
+    #     password = self.browser.find_element_by_id('password')
+    #     password.send_keys("mtate34")
+
+    #     btn = self.browser.find_element_by_xpath("//input[@type='submit']")
+
+    #     btn.click()
+
+    #     time.sleep(2)
+
+    #     alert = self.browser.switch_to.alert
+    #     self.assertEqual(alert.text, "Incorrect email or password.")
+        
 
     #def test_bid(self):
 
 
+
+if __name__ == "__main__":
+    
+
+    unittest.main()
